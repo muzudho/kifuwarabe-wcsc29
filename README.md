11bac0478f373d9b55b3d97d1f54d302c9279cab
むずでょ <muzudho1@gmail.com>
2019-04-29 08:40:39 +0900

    仕様変更☆（＾～＾）

--- README.md
+++ README.md
@@ -26,11 +26,15 @@ cd C:/muzudho/projects_rust/kifuwarabe-wcsc29/target/release
 cls
  
 ### Run.
-kifuwarabe-wcsc29 --input "C:/shogi-record/go/eating/wcsc/永世名人/01eis-kak.kif" --output "C:/muzudho/shogi-record/rpm/wcsc/永世名人/01eis-kak.rpmove"
+kifuwarabe-wcsc29 --input "C:/shogi-record/go/eating/wcsc/永世名人/01eis-kak.kif" --output "C:/muzudho/shogi-record/rpm/wcsc/永世名人/01eis-kak.rbox"
 
-kifuwarabe-wcsc29 --input "C:/shogi-record/go/eating/wcsc/１回戦/kifu.csa" --output "C:/muzudho/shogi-record/rpm/wcsc/１回戦/kifu.rpmove"
+kifuwarabe-wcsc29 --input "C:/shogi-record/go/eating/wcsc/１回戦/kifu.csa" --output "C:/muzudho/shogi-record/rpm/wcsc/１回戦/kifu.rbox"
 
 kifuwarabe-wcsc29 --input "C:/shogi-record/formation-go/wcsc28_kifu/WCSC_F1_APR_MCB.csa" --output "C:/muzudho/shogi-record/rpm-manually/WCSC_F1_APR_MCB.csa"
+
+kifuwarabe-wcsc29 --input "C:/shogi-record/eating-go/formation-go$%wcsc28_kifu$%WCSC28_F4_QHA_APR.csa" --output "C:/muzudho/shogi-record/tape-fragments/formation-go$%wcsc28_kifu$%WCSC28_F4_QHA_APR.tapefrag"
+
+kifuwarabe-wcsc29 --input "C:/shogi-record/eating-went/formation-go$%wcsc28_kifu$%WCSC28_F4_HNW_PAL.csa" --output "C:/shogi-record/eating-went/formation-go$%wcsc28_kifu$%WCSC28_F4_HNW_PAL.tapefrag"
 
 echo %ERRORLEVEL%
 ```
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ extern crate getopts;
 mod arguments;
 
 use arguments::*;
+use kifuwarabe_wcsc29_lib::application::*;
 use kifuwarabe_wcsc29_lib::*;
+use kifuwarabe_wcsc29_lib::kifu_kif::kif_tape::*;
 use kifuwarabe_wcsc29_lib::kifu_kif::kif_converter::KifConverter;
 use kifuwarabe_wcsc29_lib::kifu_csa::csa_converter::CsaConverter;
+use kifuwarabe_wcsc29_lib::kifu_csa::csa_tape::*;
+use kifuwarabe_wcsc29_lib::object_rpm::cassette_deck::*;
+use kifuwarabe_wcsc29_lib::object_rpm::cassette_tape_box::*;
+use kifuwarabe_wcsc29_lib::shogi_ban::position::*;
 use std::ffi::OsStr;
 use std::path::Path;
 
@@ -19,31 +25,57 @@ fn get_extension_from_filename(filename: &str) -> Option<&str> {
 fn main() {
     // Command line arguments.
     let args = Arguments::parse();
-
-
     let in_file = match args.input_file {
         Some(x) => x,
         None => "".to_string()
     };
-    let out_file = match args.output_file {
+    let tape_fragment_file_for_write = match args.output_file {
         Some(x) => x,
         None => "".to_string()
     };
 
-    if !in_file.is_empty() && ! out_file.is_empty() {
+    if !in_file.is_empty() && ! tape_fragment_file_for_write.is_empty() {
         // 棋譜解析。
-        let ext = get_extension_from_filename(&in_file).unwrap().to_uppercase();
+        let extension = get_extension_from_filename(&in_file).unwrap().to_uppercase();
 
-        match ext.as_str() {
+        // The application contains all immutable content.
+        let app = Application::new();
+
+        // Position.
+        let mut position = Position::new_honshogi_origin();
+
+        // Deck.
+        let mut deck = CassetteDeck::new_change(
+            Some(CassetteTapeBox::from_file(tape_fragment_file_for_write, position.get_board_size(), &app)),
+            position.get_board_size(),
+            &app
+        );
+
+        match extension.as_str() {
             "KIF" => {
-                KifConverter::convert_kif(&in_file, &out_file);
+                // Training data.
+                let ktape = KifTape::from_file(&in_file);
+
+                // Play out.
+                KifConverter::play_out_kifu_tape(&ktape, &mut position, &mut deck, &app);
+
+                // Write.
+                deck.write_tape_fragment(position.get_board_size(), &app);
             },
             "CSA" => {
-                CsaConverter::convert_csa(&in_file, &out_file);
+                // Training data.
+                let ctape = CsaTape::from_file(&in_file, &app.comm);
+
+                // Play out.
+                CsaConverter::play_out_csa_tape(&ctape, &mut position, &mut deck, &app);
+
+                // Write.
+                deck.write_tape_fragment(position.get_board_size(), &app);
             }
-            _ => {print!("Pass extension: {}", ext)}
+            _ => {print!("Pass extension: {}", extension)}
         }
     } else {
+        // 対局モード。
         main_loop();
     }
 
